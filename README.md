b907b852b32cb7c1b0f0ecb27918387b27f5ab4e
Thibault Lacroux <thibault.lacroux@gmail.com>
2017-09-10 18:18:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# MapHelpers
+
+Stolen from : https://gist.github.com/kipcole9/0bd4c6fb6109bfec9955f785087f53fb
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `map_helpers` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:map_helpers, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/map_helpers](https://hexdocs.pm/map_helpers).
---
+++ test/map_helpers_test.exs
@@ -0,0 +1,95 @@
+defmodule TestModule do
+  defstruct [:test]
+  @moduledoc """
+  Test module with struct for `MapHelpersTest`
+  """
+end
+
+defmodule MapHelpersTest do
+  @moduledoc false
+  use ExUnit.Case
+  doctest Map.Helpers
+
+  use ExUnit.Case
+
+  test ".underscore_keys" do
+    result = Map.Helpers.underscore_keys(%{"MapHelpers" => "are fun"})
+    assert result == %{"map_helpers" => "are fun"}
+  end
+
+  describe ".stringify_keys" do
+    test "not a map" do
+      result = Map.Helpers.stringify_keys("a")
+      assert result == "a"
+    end
+
+    test "with atom keys" do
+      result = Map.Helpers.stringify_keys(%{a: "b"})
+      assert result == %{"a" => "b"}
+    end
+
+    test "with multiple level" do
+      result = Map.Helpers.stringify_keys(%{a: %{b: "c"}})
+      assert result == %{"a" => %{"b" => "c"}}
+    end
+
+    test "with mixed keys" do
+      result = Map.Helpers.stringify_keys(%{:a => "b", "c" => "d"})
+      assert result == %{"a" => "b", "c" => "d"}
+    end
+  end
+
+  describe ".atomize_keys" do
+    test "not a map" do
+      result = Map.Helpers.stringify_keys("a")
+      assert result == "a"
+    end
+
+    test "with string keys" do
+      result = Map.Helpers.atomize_keys(%{"a" => "b"})
+      assert result == %{a: "b"}
+    end
+
+    test "with multiple levels" do
+      result = Map.Helpers.atomize_keys(%{"a" => %{"b" => "c"}})
+      assert result == %{a: %{b: "c"}}
+    end
+
+    test "don't do enumerable and anyway the keys are already atoms" do
+      result = Map.Helpers.atomize_keys(%TestModule{test: "test"})
+      assert result == %TestModule{test: "test"}
+    end
+  end
+
+  describe ".deep_merge" do
+    test "one level of maps without conflict" do
+      result = Map.Helpers.deep_merge(%{a: 1}, %{b: 2})
+      assert result == %{a: 1, b: 2}
+    end
+
+    test "two levels of maps without conflict" do
+      result = Map.Helpers.deep_merge(%{a: %{b: 1}}, %{a: %{c: 3}})
+      assert result == %{a: %{b: 1, c: 3}}
+    end
+
+    test "three levels of maps without conflict" do
+      result = Map.Helpers.deep_merge(%{a: %{b: %{c: 1}}}, %{a: %{b: %{d: 2}}})
+      assert result == %{a: %{b: %{c: 1, d: 2}}}
+    end
+
+    test "non-map value in left" do
+      result = Map.Helpers.deep_merge(%{a: 1}, %{a: %{b: 2}})
+      assert result == %{a: %{b:  2}}
+    end
+
+    test "non-map value in right" do
+      result = Map.Helpers.deep_merge(%{a: %{b: 1}}, %{a: 2})
+      assert result == %{a: 2}
+    end
+
+    test "non-map value in both" do
+      result = Map.Helpers.deep_merge(%{a: 1}, %{a: 2})
+      assert result == %{a: 2}
+    end
+  end
+end
